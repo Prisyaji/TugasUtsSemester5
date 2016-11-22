e4387d6948b573c126d8d1d3003537f565c6d191
Prisyaji <aji18suroso@gmail.com>
2016-11-22 16:29:48 +0700

    12141375 - dalam script diatas terdapat Arraylist yang merupakan sebuah class pada java yang digunakan untuk menyimpan koleksi data (objek) dengan tipe index Integer / angka, kemudian ada juga scanner yang merupakan perintah untuk membuat objek atau menginisialisasi object yang diinginkan pengguna atau yang diinputkan pengguna, kemudian ada juga Random yang merupakan sebuah fungsi yang dapat mengacak angka yang diinginkan.

--- src/com/mu/app12141375/Tebakan.java
+++ src/com/mu/app12141375/Tebakan.java
@@ -6,3 +6,11 @@ import java.util.Scanner;
  *
  * @author AJI
  */
+public class Tebakan {
+public static void main(String[] args) {
+        ArrayList <String> player = new ArrayList<>(); // Membuat array untuk si Pemain
+        ArrayList <Integer> score = new ArrayList<>(); // Membuat array score bagi si Pemain
+        Scanner input = new Scanner(System.in); 
+        Scanner input2= new Scanner(System.in); 
+        Random angkaRandom=new Random(); // ini untuk mengacak angka
+        String cariY; 
